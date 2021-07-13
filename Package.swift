df62d4e68be90b2e7744697d38193af523d6a000
consuelita <adelita@operatorfoundation.org>
2021-07-13 14:22:43 -0500

    Binary Library as Swift Package

--- Package.swift
+++ Package.swift
@@ -1,8 +1,18 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
-    name: "libsodium",
-    pkgConfig: "libsodium"
+    name: "Clibsodium",
+    platforms: [.macOS(.v11)],
+    products: [
+        .library(name: "Clibsodium", targets: ["ClibsodiumFramwork"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+                    name: "Clibsodium"
+                ),
+        .binaryTarget(name: "ClibsodiumFramwork", path: "Clibsodium.xcframework")
+    ]
 )
